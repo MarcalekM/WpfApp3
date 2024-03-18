8be0edc911ff47300946498a28d6ea36d329f78a
Sufi <120452154+MarcalekM@users.noreply.github.com>
2024-03-18 10:03:31 +0100

    Felzarkozas

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -20,5 +20,32 @@ namespace WpfApp3
         {
             InitializeComponent();
         }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            lbl1f.Content = ((ListBoxItem)lbx1.SelectedItem).Content;
+            lbl2f.Content = ((ComboBoxItem)cmb1.SelectedItem).Content;
+            lbx3f.Items.Clear();
+            foreach (CheckBox checkbox in stackp3f.Children)
+            {
+                if (checkbox.IsChecked == true)
+                {
+                    lbx3f.Items.Add(checkbox.Content);
+                }
+            }
+
+            foreach (RadioButton radiobutton in stackp4.Children)
+            {
+                if (radiobutton.IsChecked == true)
+                {
+                    lbl4f.Content = radiobutton.Content;
+                }
+            }
+
+            lbl5f.Content = txb5f.Text;
+
+            MessageBox.Show($"Kedves {txbNev.Text}! Kéri továbbítani az eredményeket a(z) {txbEmail.Text} címre?", "Rendelkezés", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.Yes);
+
+        }
     }
 }
